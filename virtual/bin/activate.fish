437e6bc6811899849b8db3dba1110172184d487e
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-07-28 02:31:42 +0300

    recreated the virtual environment directory and adjusted the index.html file

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mannuh/Documents/Hews-Highlight/virtual"
+set -gx VIRTUAL_ENV "/home/mannuh/Documents/News-Highlight/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
