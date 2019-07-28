f9ed42c740ef095e31e53e1296e13140646307c4
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-07-28 13:03:51 +0300

    created a comprehensive readme licence and removed the API key from config.py because it is confidential

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
 
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?country=us&apiKey=bc727edb606e4bb9a18ed13582730c86'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v1/sources?country=us&apiKey={}'
     # NEWS_API_ARTICLES_URL='https://newsapi.org/v2/everything?q=bitcoin&apiKey=bc727edb606e4bb9a18ed13582730c86'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
